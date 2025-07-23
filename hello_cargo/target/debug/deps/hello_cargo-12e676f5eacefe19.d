60fe3fe62a5ce14aaef20387f8b2953adb7d72a1
Immanuel Egboche <immanuelegboche@Immanuels-Mac-mini.local>
2025-07-23 22:45:00 +0100

    playing with cargo

---
+++ hello_cargo/target/debug/deps/hello_cargo-12e676f5eacefe19.d
@@ -0,0 +1,5 @@
+/Users/immanuelegboche/projects/hello_cargo/target/debug/deps/hello_cargo-12e676f5eacefe19.d: src/main.rs
+
+/Users/immanuelegboche/projects/hello_cargo/target/debug/deps/hello_cargo-12e676f5eacefe19: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/immanuelegboche/projects/hello_cargo/target/debug/hello_cargo: /Users/immanuelegboche/projects/hello_cargo/src/main.rs
